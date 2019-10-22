4e2682fce7823b7444f9a7f477050a1ad14d2150
jui-jamdar <56595210+jui-jamdar@users.noreply.github.com>
2019-10-22 14:10:16 +0200

    Update graph.service.ts
    Updated after removing comments

--- src/app/service/graph.service.ts
+++ src/app/service/graph.service.ts
@@ -21,32 +21,5 @@ export class GraphService {
       console.log('service called');
       return res;
     });
-
   }
-  // getAll() {
-  //   return this.http.get(this.url).subscribe(result => {
-  //     try {
-  //       // result.json();
-  //       this.tempArray = Array.of(result);
-  //       this.tempArray[0].forEach( (index) => {
-  //         console.log("item" + index);
-
-  //         index.nodes.forEach( (i,j) => {
-  //           const tnodeVertex = new NodeVertex();
-  //           tnodeVertex.nameOfVertex = i.nameOfVertex;
-  //           tnodeVertex.weight = i.weight;
-  //           this.nodeVertexArray.push(tnodeVertex);
-  //         }
-  //         );
-  //         const tempObj = new Vertex(index.name, this.nodeVertexArray, index.weight);
-  //         this.dijkstraAlgo.addVertex(tempObj);
-  //         this.nodeVertexArray = [];
-  //       });
-  //       return 'success';
-  //     } catch (e) {
-  //       return 'error';
-  //     }
-  //   });
-  // }
-
 }
